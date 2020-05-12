646040d91a388fb55df75831965a3a4a755dd9e9
Alvin <alvin.farkhan@gmail.com>
2020-05-12 07:52:23 +0700

    check exist

--- src/main/java/com/dummy/emoney/CardController.java
+++ src/main/java/com/dummy/emoney/CardController.java
@@ -3,9 +3,7 @@ package com.dummy.emoney;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,14 @@ public class CardController {
   public ResponseEntity<?> getAll(){
     List<Card> cards = cardRepository.findAll();
     return new ResponseEntity<>(cards, HttpStatus.OK);
+  }
+
+  @GetMapping("/{number}")
+  public ResponseEntity<?> checkExist(@PathVariable String number){
+    Boolean exist = cardRepository.existsByNumber(number);
+    if (!exist) {
+      return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<>(true, HttpStatus.OK);
   }
 }
